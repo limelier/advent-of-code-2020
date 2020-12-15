b76207b64fdc10477186abfb879cf373b5d31840
limelier <tudor.iacobescu.1@gmail.com>
2020-12-15 07:57:55 +0200

    Add missing space

--- 15/main.py
+++ 15/main.py
@@ -11,7 +11,7 @@ def mem_game(starting_nums, stopping_turn):
         last_num = num
         turn += 1
 
-    print(f'The number spoken on turn {stopping_turn}is {last_num}.')
+    print(f'The number spoken on turn {stopping_turn} is {last_num}.')
 
 
 def main():
